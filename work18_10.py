b29c26ea5730b88514119a52976265874d4b33ce
ILIA <ischenko.illa@lll.kpi.ua>
2022-10-18 19:35:04 +0300

    ...

--- work18_10.py
+++ work18_10.py
@@ -1,2 +1,17 @@
-print("Hello World")
-print("hhh")
+import random
+
+magicnum = random.randint(1, 10)
+
+print(magicnum)
+
+usernum = 0
+
+while magicnum != usernum:
+    usernum = int(input("Enter num from 1 to 10:"))
+    if magicnum > usernum:
+        print("bigger")
+    else:
+        print("lower")
+
+else:
+    print("Win")
