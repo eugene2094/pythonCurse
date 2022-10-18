e2d19a94aacf19c84019564834f811c9c77ca7d9
eugene2094 <70202369+eugene2094@users.noreply.github.com>
2022-10-18 19:26:13 +0300

    our work for 18.10 update

--- classWork18_10.py
+++ classWork18_10.py
@@ -1,1 +1,43 @@
-print("hello")
+num = 0
+
+# while num < 10:
+#     if num == 5:
+#         break
+#     print(num)
+#     num += 1
+
+# while num < 50:
+#     num += 1
+#     if num % 2 == 0:
+#         continue
+#     else:
+#         print(num)
+
+
+# usernum = 0
+#
+# while True:
+#     usernum = int(input("enter num"))
+#     if usernum == 0:
+#         break
+#     elif usernum < 0:
+#         continue
+#     print(usernum)
+#
+
+import random
+
+magicNumber = random.randint(1, 10)
+
+print(magicNumber)
+
+usernum = 0
+
+while magicNumber != usernum:
+    usernum = int(input("enter num from 1 to 10 - > "))
+    if magicNumber > usernum:
+        print("try bigger num ")
+    else:
+        print("try lower num ")
+else:
+    print("You win !")
